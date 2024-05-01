1ac6a34e447b4a9a1419672d8b05e12ceb91d282
Antonio Vuono <antoniosvuono@icloud.com>
2024-05-01 20:31:25 -0300

    Initial Commit

---
+++ api-calls-swift-01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  api-calls-swift-01
+//
+//  Created by Antonio Vuono on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ api-calls-swift-01/api_calls_swift_01App.swift
@@ -0,0 +1,17 @@
+//
+//  api_calls_swift_01App.swift
+//  api-calls-swift-01
+//
+//  Created by Antonio Vuono on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct api_calls_swift_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
